d658f666143b72af7167dfd13b7a7206221de633
Bmercado17 <46335239+Bmercado17@users.noreply.github.com>
2022-09-08 21:43:45 -0700

    Add files via upload

---
+++ components/FriendContact.vue
@@ -0,0 +1,72 @@
+<template>
+  <li>
+    <h2>{{name}} {{isFavorite? '(favorite)':''}}</h2>
+    <button @click="toggleFavorite">Favorite</button>
+    <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong>{{phoneNumber}}</li>
+        <li><strong>Email:</strong>{{emailAddress}}</li>
+    </ul>
+    <button @click="$emit('delete',id)">Delete</button>
+  </li>
+</template>
+//working with git to get this published
+<script>
+export default {
+    emits:['toggle-favorite','delete'],
+    // emits: {
+    //     'toggle-favorite':function(id){
+    //         if(id){
+    //             return true;
+    //         }else{
+    //             warn('id is missing biatch')
+    //             return false;
+    //         }
+    //     }
+    // },
+    
+    props:{
+        id: {
+            type: String,
+            required: true,
+        },
+        name:{
+            type:String,
+            required:true,
+        },
+        phoneNumber:{
+            type: String,
+            required: true,
+        },
+        emailAddress: {
+            type: String,
+            required: true
+        },
+        isFavorite: {
+            type: Boolean,
+            required: false,
+            default:false,
+          
+        },
+
+    },
+    data(){
+        return{
+            
+            detailsAreVisible: false,
+        }  
+    },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible;
+        },
+        // '$emit' to communicate an event with the parent file app.vue
+         toggleFavorite(){
+            this.$emit('toggle-favorite',this.id)
+        }
+        
+    },   
+    
+};
+</script>
+
---
+++ components/NewFriend.vue
@@ -0,0 +1,39 @@
+<template>
+    <form @submit.prevent="submitData">
+     <div>
+        <label>Name</label>   
+        <input v-model="enteredName" type="text">
+     </div>
+     <div>
+        <label>Phone</label>   
+        <input v-model="enteredPhone" type="tel">
+     </div>
+     <div>
+        <label>Email Address</label>   
+        <input v-model="enteredEmail" type="email">
+     </div>
+     <button >Add Contact</button>
+    </form>
+</template>
+
+<!-- logic -->
+<script>  
+ export default {
+    emits: ['add-contact'],
+    data(){
+        return{
+            enteredName: '',
+            enteredPhone: '',
+            enteredEmail: ''
+        };
+    },
+    methods: {
+        submitData(){
+            this.$emit('add-contact',
+            this.enteredName,
+            this.enteredPhone,
+            this.enteredEmail)
+        }
+    }
+ }
+</script>
